53e9c13fa0f27f65e16eec2c91e28d4afa4ba2ea
417MahirRahman <35mahirrahman@gmail.com>
2025-11-11 17:12:27 +0600

    Created Update-Details API

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ async function run() {
         })
     })
 
-    app.put('/demo/:id', async(req, res) => {
+    app.put('/availableFoods/:id', async(req, res) => {
         const {id} = req.params
         const data = req.body
         const objectId = new ObjectId(id)
@@ -64,7 +64,7 @@ async function run() {
         const update = {
             $set: data
         }
-        const result = await collection.updateOne({filter, update})
+        const result = await foodCollection.updateOne({filter, update})
 
         res.send({
             success: true,
