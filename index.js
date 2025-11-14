1635154366d8c2136ae92f8a12e6b9e6f10d3fed
417MahirRahman <35mahirrahman@gmail.com>
2025-11-15 03:40:45 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const cors = require('cors')
-require("dotenv").config()
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8rpbzhd.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -16,8 +22,30 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+//middleware
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({
+      message: "Token not found.",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    await admin.auth().verifyIdToken(token);
+    next();
+  } catch {
+    res.status(401).send({
+      message: "Token not found.",
+    });
+  }
+};
 
 async function run() {
   try {
@@ -25,78 +53,199 @@ async function run() {
     await client.connect();
 
     //dataBase, Collection
-    const dataBase = client.db('Plate_Share')
-    const foodCollection = dataBase.collection('Foods')
+    const database = client.db("Plate_Share");
+    const foodCollection = database.collection("Foods");
+    const foodRequestCollection = database.collection("Food_Request");
 
-    app.get('/availableFoods', async(req, res) => {
-        
-        const result = await foodCollection.find().toArray()
+    // Get all available foods
+    app.get("/availableFoods", async (req, res) => {
+      const result = await foodCollection.find().toArray();
 
-        res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.get('/availableFoods/:id', async(req, res) => {
-        const {id} = req.params
-        const food = await foodCollection.findOne({_id: new ObjectId(id)})
+    // Get a specific food by ID
+    app.get("/availableFoods/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const food = await foodCollection.findOne({ _id: new ObjectId(id) });
 
-        res.send({
-            success: true,
-            food
-        })
-    })
+      res.send({
+        success: true,
+        food,
+      });
+    });
 
-    app.get('/myFood', async (req, res) => {
-      const email = req.query.email
-      const result = await foodCollection.find({upLoadedBy: email}).toArray()
+    //Dynamic-Food
+    app.get("/dynamicFood", async (req, res) => {
+      const result = await foodCollection
+        .find()
+        .sort({ quantity: "desc" })
+        .limit(6)
+        .toArray();
 
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.post('/availableFoods', async(req, res) => {
-        const data = req.body
-        const result = await foodCollection.insertOne(data)
+    // Get all foods posted by a specific user
+    app.get("/myFood", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const result = await foodCollection
+        .find({ donatorEmail: email })
+        .toArray();
 
-        res.send({
-            success: true,
-            result
-        })
-    })
+      res.send(result);
+    });
 
-    app.put('/availableFoods/:id', async(req, res) => {
-        const {id} = req.params
-        const data = req.body
-        const objectId = new ObjectId(id)
+    // Get food requests made by the user
+    app.get("/FoodRequest", verifyToken, async (req, res) => {
+      const result = await foodRequestCollection.find().toArray();
 
-        const filter = {_id: objectId}
-        const update = {
-            $set: data
-        }
-        const result = await foodCollection.updateOne({filter, update})
+      res.send(result);
+    });
 
-        res.send({
-            success: true,
-            result
-        })
-    })
+    // Get a specific food request by ID
+    app.get("/FoodRequest/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const food = await foodRequestCollection.findOne({
+        _id: new ObjectId(id),
+      });
 
-    app.delete('/availableFoods/id', async(req, res) => {
-        const {id} = req.params
-        const objectId = new ObjectId(id)
-        const filter = {_id: objectId}
+      res.send({
+        success: true,
+        food,
+      });
+    });
 
-        const result = await foodCollection.deleteOne({filter})
+    // Get food requests sent by the user (as owner)
+    app.get("/myFoodRequests/", verifyToken, (req, res) => {
+      const email = req.query.email;
+      if (!email)
+        return res.send({ success: false, message: "Email is required" });
 
-        req.send({
-            success: true,
-            result
-        })
-    })
+      foodRequestCollection
+        .find({ Email: email })
+        .toArray()
+        .then((requests) => res.send({ success: true, requests }))
+        .catch(() =>
+          res.send({
+            success: false,
+            message: "Failed to retrieve food requests",
+          })
+        );
+    });
 
+    // Post a new available food
+    app.post("/availableFoods", verifyToken,  async (req, res) => {
+      const data = req.body;
+      const result = await foodCollection.insertOne(data);
 
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    // Post a new food request
+    app.post("/FoodRequest", verifyToken, async (req, res) => {
+      const data = {
+        Name: req.body.Name,
+        Email: req.body.Email,
+        ImageURL: req.body.ImageURL,
+        foodID: req.body.foodID,
+        foodname: req.body.foodname,
+        foodOwnerEmail: req.body.foodOwnerEmail,
+        ContactNumber: req.body.ContactNumber,
+        foodStatus: "Pending",
+      };
+
+      const result = await foodRequestCollection.insertOne(data);
+
+      res.send({
+        success: true,
+        message: "Food request saved successfully",
+        insertedId: result.insertedId,
+      });
+    });
+
+    // Update an available food by ID
+    app.put("/availableFoods/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+
+      const updatedData = {
+        foodName: req.body.foodName,
+        foodImage: req.body.foodImage,
+        quantity: Number(req.body.quantity),
+        pickupLocation: req.body.pickupLocation,
+        expireDate: req.body.expireDate,
+        additionalNote: req.body.additionalNote,
+      };
+
+      Object.keys(updatedData).forEach(
+        (key) => updatedData[key] === undefined && delete updatedData[key]
+      );
+
+      const result = await foodCollection.findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: updatedData },
+        { returnDocument: "after" }
+      );
+
+      return res.send({
+        success: true,
+        message: "Food updated successfully",
+        updatedFood: result.value,
+      });
+    });
+
+    // Update FoodRequest status
+    app.put("/FoodRequest/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const { foodStatus } = req.body;
+
+      const objectId = new ObjectId(id);
+
+      const result = await foodRequestCollection.findOneAndUpdate(
+        { _id: objectId },
+        { $set: { foodStatus } },
+        { returnDocument: "after" }
+      );
+
+      res.send({ success: true, updatedRequest: result.value });
+    });
+
+    // Delete an available food by ID
+    app.delete("/availableFoods/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+
+      const result = await foodCollection.deleteOne(filter);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    // Delete an food-request food by ID
+    app.delete("/FoodRequest/:id", verifyToken, async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+
+      const result = await foodRequestCollection.deleteOne(filter);
+
+      res.send({
+        success: true,
+        result,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -104,11 +253,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-  console.log(`The Server is Running on port ${port}`)
-})
+  console.log(`The Server is Running on port ${port}`);
+});
